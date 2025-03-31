3433f9e3f5783e39da5c0618128921ae247d7288
unknown <ra200246@ACAD.UCDB.BR>
2025-03-31 12:29:16 -0400

    (feat): Added CQRS

--- TravelInspiration.API/Shared/Behaviours/HandlePerformanceMetricBehaviour.cs
+++ TravelInspiration.API/Shared/Behaviours/HandlePerformanceMetricBehaviour.cs
@@ -1,6 +1,29 @@
-﻿namespace TravelInspiration.API.Shared.Behaviours
+﻿using System.Diagnostics;
+using MediatR;
+using TravelInspiration.API.Shared.Metrics;
+
+namespace TravelInspiration.API.Shared.Behaviours
 {
-    public class HandlePerformanceMetricBehaviour
+    public sealed class HandlePerformanceMetricBehaviour<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
     {
+        private readonly HandlePerformanceMetric _handlePerformanceMetric;
+        private readonly Stopwatch _timer = new();
+
+        public HandlePerformanceMetricBehaviour(HandlePerformanceMetric handlePerformanceMetric)
+        {
+            _handlePerformanceMetric = handlePerformanceMetric;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            _timer.Start();
+            var respose = await next();
+            _timer.Stop();
+
+            _handlePerformanceMetric.MiliSecondsElapsed(_timer.ElapsedMilliseconds);
+
+            return respose;
+        }
     }
 }
